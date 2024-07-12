7fbf324191af8d7cbd25206858d9e51b2a4c73d7
Nicolas Molina <hi@nicobytes.com>
2024-07-12 09:38:05 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Calculator } from './utils/calculator';
-import { FooterComponent } from './components/footer/footer.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { BannerComponent } from './components/banner/banner.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet />',
   standalone: true,
-  imports: [BannerComponent, RouterLink, RouterOutlet, FooterComponent],
+  imports: [ RouterOutlet],
 })
-export class AppComponent implements OnInit {
-  title = 'ng-testing-services';
-
-  ngOnInit() {
-    const calculator = new Calculator();
-    const rta = calculator.multiply(3, 3);
-    // console.log(rta === 9);
-    const rta2 = calculator.divide(3, 0);
-    // console.log(rta2 === null);
-  }
-}
+export class AppComponent {}
